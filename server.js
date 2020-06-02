b6c076e14e44d247a15d21791f4eb7046d764a92
roseroyeicok@gmail.com <roseroyeicok@gmail.com>
2020-06-02 15:32:51 -0500

    Version 1.0

--- server.js
+++ server.js
@@ -2,20 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/un-nuevo-amanecer'));
+app.use(express.static('./dist/UnNuevoAmanecer'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/un-nuevo-amanecer/'}),
-);
-
-app.listen(process.env.PORT || 8080);const express = require('express');
-
-const app = express();
-
-app.use(express.static('./dist/un-nuevo-amanecer'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/un-nuevo-amanecer/'}),
+    res.sendFile('index.html', {root: 'dist/UnNuevoAmanecer/'}),
 );
 
 app.listen(process.env.PORT || 8080);
